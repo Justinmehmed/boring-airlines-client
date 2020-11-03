45426efc68c70d6afa5c9ea604d3e1a415cc2d37
Bastien Durand <basilexpeditions@gmail.com>
2020-11-03 21:45:14 +1100

    Components

---
+++ src/components/Airline.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Flight extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Flight Information</h1>
+      </div>
+    )
+  }
+}
+
+export default Flight;
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Airline from './Airline'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Airline />
+    </div>
+  );
+}
+
+export default App;
