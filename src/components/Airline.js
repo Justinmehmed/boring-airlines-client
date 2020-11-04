1a99c2d4012b7c8b88627c86687194deabbf6971
sarahhae <helloitssarahhae@gmail.com>
2020-11-05 12:24:58 +1300

    bug fix

--- src/components/Airline.js
+++ src/components/Airline.js
@@ -15,12 +15,9 @@ class Flight extends Component {
   render() {
     return(
       <div id="border-outline">
-<<<<<<< HEAD
       <SeatDiagram />
-=======
       <h3>Select a seat:</h3>
       <PlaneDiagram />
->>>>>>> 658af1dbc1d5f002ec0faf40f43207a2862a2fca
       <ReserveSeat />
       <Seats />
       </div>
--- src/components/ToAndFrom.js
+++ src/components/ToAndFrom.js
@@ -1,50 +1,7 @@
 import React, { Component } from 'react';
-
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
-
-import ListOfFlights from './ListOfFlights'
-
-const options = [
-  'Sydney', 'Melbourne', 'Adelaide', 'Perth'
-]
+import ListOfFlights from './ListOfFlights';
 
 class ToAndFrom extends Component {
-  // function for value
-
-  constructor() {
-    super()
-    this.state = {
-      to: '',
-      from: '',
-    }
-    this._handleSubmit = this._handleSubmit.bind(this);
-  }
-
-  _handleSubmit(event) {
-    event.preventDefault();
-    this.setState({ to: '', from: '' })
-  }
-
-  render() {
-    return(
-      <div id="ToAndFromContainer">
-      <form onSubmit={ this._handleSubmit }>
-
-      <div id="dropDown">
-      <Dropdown options={options} onChange={this._onSelect} value={this.state.to}/>
-      </div>
-
-      <div id="dropDown">
-      <Dropdown options={options} onChange={this._onSelect} value={this.state.from}/>
-      </div>
-
-      <button type="submit" id="ToAndFromSubmit">Check for flights</button>
-
-      </form>
-
-class ToAndFrom extends Component {
-
 constructor() {
     super();
     this.state = {
@@ -52,7 +9,6 @@ constructor() {
     }
     this._handleSubmit = this._handleSubmit.bind(this)
   }
-
   _handleSubmit(event) {
     event.preventDefault();
     console.log('Button submitted!');
@@ -71,5 +27,4 @@ constructor() {
     )
   }
   }
-
 export default ToAndFrom;
