a275d8a6a16b16583253716c0d3d2d85439f933e
Bastien Durand <basilexpeditions@gmail.com>
2020-11-04 11:38:35 +1100

    Added dropdown forTo and from

--- src/components/ToAndFrom.js
+++ src/components/ToAndFrom.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css'
+
+const options = [
+  'Sydney', 'Melbourne', 'Adelaide', 'Perth'
+]
+
+const fromSydney = [
+  'Melbourne', 'Adelaide', 'Perth'
+];
+
+const fromMelbourne = [
+  'Sydney', 'Adelaide', 'Perth'
+];
+
+const fromAdelaide = [
+  'Sydney', 'Melbourne', 'Perth'
+];
 
 class ToAndFrom extends Component {
+
+
   render() {
     return(
-      <div id="ToAndFrom">
-      <h3>This is the to and from like in the example (We should just use 3 random dummy data sets like in their example as an MVP)</h3>
-        <form>
-          <input placeholder="To"></input>
-          <input placeholder="From"></input>
-        </form>
+      <div id="ToAndFromContainer">
+        <div id="dropDown">
+        <Dropdown options={options} onChange={this._onSelect} value="To"
+         />
+        </div>
+        <div id="dropDown">
+        <Dropdown options={options} onChange={this._onSelect} value="From"
+         />
+        </div>
+        <button id="ToAndFromSubmit">Check for flights</button>
       </div>
     )
   }
