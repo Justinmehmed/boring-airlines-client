4156b7f5246a56b0f524a1495e05ef71272fa039
Bastien Durand <basilexpeditions@gmail.com>
2020-11-05 13:31:17 +1100

    Flight data

--- src/components/ListOfFlights.js
+++ src/components/ListOfFlights.js
@@ -30,6 +30,14 @@ _handleSubmit(event) {
             <p><strong>1/1/13</strong> SYD > JFK 18:00 <button onClick={this._handleSubmit}>✓</button></p>
           </div>
 
+          <div>
+            <p><strong>3/1/13</strong> SYD > JFK 06:00 <button onClick={this._handleSubmit}>✓</button></p>
+          </div>
+
+          <div>
+            <p><strong>5/1/13</strong> SYD > JFK 12:00 <button onClick={this._handleSubmit}>✓</button></p>
+          </div>
+
         </form>
         {this.state.flightSelected && <Airline /> }
       </div>
